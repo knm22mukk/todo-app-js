5cbf6147967872c5442591f3da42a55d8ce3eb74
keita.nakamura <76908302+knm22mukk@users.noreply.github.com>
2022-07-31 21:39:06 +0900

    タスクの完了ボタンの機能追加

--- src/index.js
+++ src/index.js
@@ -20,6 +20,28 @@ const onClickAdd = () => {
   completeButton.addEventListener("click", () => {
     //　押された完了ボタンの親要素を未完了リストから削除
     deleteFromIncompleteList(completeButton.parentNode);
+
+    //　完了リストに追加する要素の取得
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    addTarget.textContent = null;
+
+    // 完了リストを生成
+    const completeTodo = document.createElement("div");
+    completeTodo.innerText = text;
+
+    //buttonタグの生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // 各要素を必要な場所に設定
+    addTarget.appendChild(completeTodo);
+    addTarget.appendChild(backButton);
+
+    //　完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(削除)タグ生成
